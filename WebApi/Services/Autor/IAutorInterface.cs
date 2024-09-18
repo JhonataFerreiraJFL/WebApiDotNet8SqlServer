1dd59a1272b579430d0da9911f8d8879667f4339
Jhonata Ferreira <60432652+JhonataFerreiraJFL@users.noreply.github.com>
2024-09-18 11:48:35 -0300

    Add project files.

---
+++ WebApi/Services/Autor/IAutorInterface.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebApi.Controllers;
+using WebApi.Dto.Autor;
+using WebApi.Dto.Livro;
+using WebApi.Models;
+
+namespace WebApi.Services.Autor
+{
+    public interface IAutorInterface
+    {
+        Task<ResponseModel<AutorModel>> BuscarAutorPorId(int idAutorId);
+        Task<ResponseModel<AutorModel>> BuscarAutorIdLivro(int idLivro);
+        Task<ResponseModel<List<AutorModel>>> ListarAutores();
+        Task<ResponseModel<List<AutorModel>>> CriarAutor(AutorCriacaoDto autorCriacaoDto);
+        Task<ResponseModel<List<AutorModel>>> EditarAutor(AutorEdicaoDto autorEdicaoDto);
+        Task<ResponseModel<List<AutorModel>>> ExcluirAutor(int idLivro);
+
+    }
+}
